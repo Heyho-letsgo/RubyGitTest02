c371da0456a3180028e319f3b3651c0278576079
Andre Baudin-Laurencin <abaudin@gmail.com>
2013-09-11 15:05:18 +0200

    Juste des modifs...

--- 001Hello.rb
+++ 001Hello.rb
@@ -12,8 +12,12 @@ puts 2+3*4
  # en math la multiplication ou la division prennent le pas sur l'adition ou la soustraction. c'est doncquivalent
  # à 2+(3*4) = 14 . Il est recommandé de mettre des paraenthèse pour plus de clarté
 
+
 puts "Calcul du nombre de secondes dans une journee"
 
 puts "seconds per a day is #{3600*24}"
 # Le fait de mettre une dièze suivi de deux accolades permet de dire au programme :  "Sort du string et passe en calcul pour les valeurs en accolades"
 
+puts "ho " "ho " "ho " "Merry Christmas"
+
+puts "Ho " * 3 + 'Merry Christmas'
