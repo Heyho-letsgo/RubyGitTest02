5cc36abed41eaa7d26cb5871c82ef65dd756a685
Andre Baudin-Laurencin <abaudin@gmail.com>
2013-09-11 15:09:02 +0200

    Juste des modifs...

---
+++ hello0011.rb
@@ -0,0 +1,26 @@
+puts "Hello World"                  # Tout ce qui est  entre des quotes est considéré comme de l'alpha-numérique
+
+puts "Andy"                         # The puts method met par défaut un retour à la ligne
+
+puts "------------------------"
+print "Hello world "                # The print method ne met pas de return key
+puts "Andy"
+
+puts "------------------------"
+
+puts 2+3*4
+                                    # en math la multiplication ou la division prennent le pas sur l'adition ou la soustraction. c'est doncquivalent
+                                    # à 2+(3*4) = 14 . Il est recommandé de mettre des paraenthèse pour plus de clarté
+
+
+puts "Calcul du nombre de secondes dans une journee"
+
+puts "seconds per a day is #{3600*24}"
+                                    # Le fait de mettre une dièze suivi de deux accolades permet de dire au programme :  "Sort du string et passe en calcul pour les valeurs en accolades"
+
+puts "Ecrire ho ho ho Merry Christmas"
+
+puts "ho " "ho " "ho " "Merry Christmas"
+
+puts "Ho " * 3 + 'Merry Christmas'
+
