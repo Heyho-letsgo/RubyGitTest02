d04765288534672687075d23c58b752beb5247ff
Andre Baudin-Laurencin <abaudin@gmail.com>
2013-09-11 14:16:39 +0200

    Changement de dossier

---
+++ 001Hello.rb
@@ -0,0 +1,12 @@
+puts "Hello World"                  # Tout ce qui est  entre des quotes est considéré comme de l'alpha-numérique
+
+puts "Andy"                         # The puts method met par défaut un retour à la ligne
+
+puts "------------------------"
+print "Hello world "                # The print method ne met pas de return key
+puts "Andy"
+
+puts "------------------------"
+
+puts 2 + 3 * 4                      # en math la multiplication ou la division prennent le pas sur l'adition ou
+                                    # la soustraction. c'est don équivalent à 2+(3*4)
